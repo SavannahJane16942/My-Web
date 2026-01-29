9c32515f2ddf5e0317c36c16797faa2995ab3606
Savannah Jane Ducusin <savannah.ducusin@globe.com.ph>
2026-01-29 13:25:11 +0800

    Added few Home Page Layout.

--- my-profile/src/code/App.jsx
+++ my-profile/src/code/App.jsx
@@ -1,15 +1,39 @@
 import { useState } from 'react'
 import '../style/App.css'
+import bgHome from '../assets/bg.jpeg'
+import prof from '../assets/prof.jpeg'
 
 function App() {
 
   return (
     <>
       <section id='sec_1'>
+
         <div className='home'>
-          <div className="hdiv left_div"></div>
-          <div className="hdiv mid_div"></div>
-          <div className="hdiv right_div"></div>
+          <div className="bgdiv">
+            <img className='bg_home' src={bgHome}></img>
+          </div>
+
+        <div className="profdiv">
+          <div className="profdiv_ div_img">
+            <img className='profImg' src={prof}></img>
+          </div>
+          <div className="profdiv_ div_info">
+            <div className="info_hldr name_">Sav Ducusin</div>
+            <div className="info_hldr cap">Everything happens for a reason.</div>
+            <div className="info_hldr odr_dt">
+              <span className="odr_dt_con">
+                <img className='icon_' src='image.jpeg'></img> San Fernando City, La Union
+              </span>
+              <span className="odr_dt_con">
+                <img className='icon_' src='image.jpeg'></img> DMMMMSU-MLUC
+              </span>
+              <span className="odr_dt_con">
+                <img className='icon_' src='image.jpeg'></img> @sav.ducusin
+              </span>
+            </div>
+          </div>
+        </div>
         </div>
       </section>
     </>
